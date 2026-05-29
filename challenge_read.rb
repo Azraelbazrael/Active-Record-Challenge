280530279e814964331e59042c429855e939b5e3
azrael <casilanazrael@gmail.com>
2026-05-29 00:37:13 -0500

    fix stuff

--- challenge_read.rb
+++ challenge_read.rb
@@ -1,5 +1,9 @@
 require_relative 'ar.rb'
 
-number_of_products = Product.count
 
+number_of_products = Product.count
+low_stock = Product.where("stock_quantity < ?", 5).count 
+c_product_above_ten = Product.where("name LIKE ? AND price >= ?", "C" + "%", 10).pluck(:name)
 puts "There are #{number_of_products} in the products table."
+puts "The number of products with the least amount of stock are: #{low_stock}."
+puts "Products starting at 10 dollars are: #{c_product_above_ten}."
--- models/product.rb
+++ models/product.rb
@@ -4,5 +4,10 @@ class Product < ActiveRecord::Base
   # A product has a many to one relationship with a category.
   # The products table has a category_id foreign key.
   # In other words, a product belongs to a category.
+
+  # A product table contains: 
+  # a Product ID, a name, a description, a price, stock quantity, a category id, 
+  # a date when the product was created at and when it was last updated
+
   belongs_to :category
 end
